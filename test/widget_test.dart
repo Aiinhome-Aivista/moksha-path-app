1e7f616e636a70974cf2428484265d1aa13ae54e
Himanshu <294himangshu@gmail.com>
2026-02-05 11:35:16 +0530

    all fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
-import 'package:moksha_path/moksha_path.dart';
+import 'package:moksha_path/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
