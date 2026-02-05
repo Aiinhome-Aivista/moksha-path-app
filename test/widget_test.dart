b52bf01c4f1d0b35649b67e7e7ac0361f71d4e62
Arendu Chanda <arenduchanda1997@gmail.com>
2026-02-05 18:17:05 +0530

    all conflict solved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
-import 'package:moksha_path/main.dart';
+import 'package:moksha_path/moksha_path.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
